f1933786908732c9f0a9aa2e9aaff579cf8c6430
AllenSyn20 <andrewholden1049@gmail.com>
2025-02-13 12:03:57 -0600

    final fix

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,7 @@ class _FirstPageState extends State<FirstPage> {
               max: 1.0,
             ),
             SizedBox(height: 20),
-            // Dog Image with error handlingsls
+            // Dog Image with error handlings
             Image.network(
               'https://images.dog.ceo/breeds/labrador/n02099712_7418.jpg',
               width: 200,
